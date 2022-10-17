0bc693bb98bb9997d64dc08b691b316c7da2f554
Xsodex_app <Flex513513@gmail.com>
2022-10-17 15:36:03 +0300

    Сделал базу для генерации доков

--- client/src/app/booking/components/docs/docs.component.ts
+++ client/src/app/booking/components/docs/docs.component.ts
@@ -1,7 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Booking } from 'src/app/shared/types/interfaces';
 import { BookingsService } from '../../services/bookings.service';
+import pdfMake from "pdfmake/build/pdfmake";
+import pdfFonts from "pdfmake/build/vfs_fonts";
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
+import htmlToPdfmake from 'html-to-pdfmake';
+
 
 @Component({
   selector: 'app-docs',
@@ -11,6 +16,7 @@ import { BookingsService } from '../../services/bookings.service';
 export class DocsComponent implements OnInit {
   bookingId!: string;
   actualBooking!: Booking;
+  @ViewChild('content') content!: ElementRef;
 
   constructor(
     private bookings: BookingsService,
@@ -28,5 +34,19 @@ export class DocsComponent implements OnInit {
       this.actualBooking = res;
     });
   }
+
+  generatePDF() {
+
+    var html = htmlToPdfmake(this.content.nativeElement.innerHTML);
+    
+    let docDefinition = {
+      content: html
+    };
+
+    
+    pdfMake.createPdf(docDefinition).open();
+
+  } 
+
 
 }
